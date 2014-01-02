1580b4a46b085ef8c0055af127abe78af84f420f
Eran Z <eran@zimbler.net>
2014-01-02 18:44:11 +0200

    removing sys.exit from sensei for the koan_runner to work as required

--- python2/runner/sensei.py
+++ python2/runner/sensei.py
@@ -92,7 +92,7 @@ class Sensei(MockableTestResult):
         self.stream.writeln("")
         self.stream.writeln(self.say_something_zenlike())
 
-        if self.failures: sys.exit(-1)
+        if self.failures: return
         self.stream.writeln(
             "\n{0}**************************************************" \
             .format(Fore.RESET))
